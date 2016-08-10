552d34fe1fb19177c454bba4952f7ae284a223ac
Pós <Ps@DESKTOP-SUH8QJN>
2016-08-10 08:32:30 -0300

    Versão 1

---
+++ teste.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+ 
+
+  <style type="text/css">
+    h1 { 
+      font:normal 2.4em/1.6 georgia, "times new roman", "bitstream vera serif", times, serif;
+      color:#900;
+    }
+  </style>
+  
+</head>
+<body>
+
+</body>
+</html>
